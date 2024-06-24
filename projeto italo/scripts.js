c97ab412c887868e055cb9cac1f42f07e28f085f
Samuel-Oripes <moreiraoripessamuelvictor@gmail.com>
2024-06-24 09:12:47 -0300

    adicionado transicao

--- projeto italo/scripts.js
+++ projeto italo/scripts.js
@@ -6,42 +6,64 @@ const cronograma = document.querySelector(".calendario")
 
 function chamarCronograma(){
     cronograma.style.visibility = "visible"
+    cronograma.style.top = "65px"
     perfil.style.visibility = "hidden"
     menu.style.visibility = "hidden"
     lembrete.style.visibility = "hidden"
     mascaraForm.style.visibility = "visible"
+    perfil.style.left = "-400px"
+    lembrete.style.top = "-200px"
+    menu.style.right = "-200px"
 }
 
 function chamarLembrete(){
     lembrete.style.visibility = "visible"
+    lembrete.style.top = "70px"
     mascaraForm.style.visibility = "visible"
     perfil.style.visibility = "hidden"
     menu.style.visibility = "hidden"
     cronograma.style.visibility = "hidden"
+    perfil.style.left = "-400px"
+    menu.style.right = "-200px"
+    cronograma.style.top = "-300px"
 }
 
 function chamarMenu(){
     menu.style.visibility = "visible"
+    menu.style.right = "0"
     mascaraForm.style.visibility = "visible"
     perfil.style.visibility = "hidden"
     lembrete.style.visibility = "hidden"
     cronograma.style.visibility = "hidden"
+    perfil.style.left = "-400px"
+    lembrete.style.top = "-200px"
+    cronograma.style.top = "-300px"
 }
 
 function chamarPerfil(){
     perfil.style.visibility = "visible"
+    perfil.style.left = "0"
     mascaraForm.style.visibility = "visible"
     menu.style.visibility = "hidden"
     lembrete.style.visibility = "hidden"
     cronograma.style.visibility = "hidden"
+    lembrete.style.top = "-200px"
+    menu.style.right = "-200px"
+    cronograma.style.top = "-300px"
 }
 
 function fecharAba(){
     mascaraForm.style.visibility = "hidden"
-    perfil.style.visibility = "hidden"
-    menu.style.visibility = "hidden"
-    lembrete.style.visibility = "hidden"
-    cronograma.style.visibility = "hidden"
+    setTimeout(function(){
+        perfil.style.visibility = "hidden"
+        menu.style.visibility = "hidden"
+        lembrete.style.visibility = "hidden"
+        cronograma.style.visibility = "hidden"
+    },1000)
+    perfil.style.left = "-400px"
+    lembrete.style.top = "-200px"
+    menu.style.right = "-200px"
+    cronograma.style.top = "-300px"
 }
 
 let calendario = document.querySelector(".numero-dias")
